205da4ce014b0828191a95d9647f4378813142d7
JohnnTech <jonntech01@gmail.com>
2025-04-29 23:49:59 -0300

    feat: add styled borders sm and improve form input design in FormsContact

--- src/components/landingPage/FormsContact.tsx
+++ src/components/landingPage/FormsContact.tsx
@@ -16,12 +16,12 @@ function FormsContact() {
     >
       <div className="flex flex-col gap-2 md:gap-2 w-full px-4 md:px-8 lg:px-20 py-6 md:py-12 relative">
         <div
-          className="relative w-full h-60 sm:h-[300px] md:h-[400px] z-10 -translate-y-1/2 md:-translate-x-1/2 md:translate-x-0 rounded-lg overflow-hidden"
+          className="relative w-full border-1 border-gray-300 md:border-0 h-60 sm:h-[300px] md:h-[400px] z-10 -translate-y-1/2 md:-translate-x-1/2 md:translate-x-0 rounded-lg overflow-hidden"
           style={{
             backgroundColor: colors?.background['background-primary'],
           }}
         >
-          <div className="hidden md:block w-full h-full">
+          <div className="hidden md:block w-full h-full ">
             <ImageFromJSON
               src={support?.image}
               alt={support?.alt}
