b7203a29e83f5d21dfcb0601dee0e464b00834c4
marioFLS <mariofernandolsantos@gmail.com>
2025-06-23 18:51:19 -0300

    feat: change main layout in pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,23 @@
 'use client'
 
-import MainLayout from '@/components/layout/MainLayout'
 import { useContext } from 'react'
 import { ConfigContext } from '@/contexts/ConfigContext'
 import ImageFromJSON from '@/components/core/ImageFromJSON'
 import LoginForm from '@/app/login/FormsLogin'
+import Footer from '@/components/common/footer'
+import Header from '@/components/landingPage/Header'
 
 export default function LoginPage() {
   const { texts } = useContext(ConfigContext)
   const loginData = texts?.['register']
 
   return (
-    <MainLayout disablePadding>
-      <div className="flex flex-col min-h-screen">
-        <main className="flex flex-1">
-          {/* Lado da imagem */}
-          <div
-            className="
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        {/* Lado da imagem */}
+        <div
+          className="
               hidden md:block
               w-[300px]
               sm:w-[350px]
@@ -28,30 +29,30 @@ export default function LoginPage() {
               relative
               image-large-override
             "
-          >
-            <ImageFromJSON
-              src={loginData?.image}
-              alt={loginData?.alt}
-              className="w-full h-auto object-contain"
-              width={653}
-              height={800}
-            />
-          </div>
+        >
+          <ImageFromJSON
+            src={loginData?.image}
+            alt={loginData?.alt}
+            className="w-full h-auto object-contain"
+            width={653}
+            height={800}
+          />
+        </div>
 
-          {/* Lado dos steps */}
-          <div className="flex-1 flex items-start justify-start px-4 sm:px-8 md:px-12 pt-12 pb-8">
-            <div
-              className="
+        {/* Lado dos steps */}
+        <div className="flex-1 flex items-start justify-start px-4 sm:px-8 md:px-12 pt-12 pb-8">
+          <div
+            className="
                 mt-4
                 ml-6 sm:ml-10 md:ml-14 lg:ml-20
                 max-w-md w-full
               "
-            >
-              <LoginForm />
-            </div>
+          >
+            <LoginForm />
           </div>
-        </main>
-      </div>
-    </MainLayout>
+        </div>
+      </main>
+      <Footer />
+    </div>
   )
 }
--- src/app/register-metamask/page.tsx
+++ src/app/register-metamask/page.tsx
@@ -1,22 +1,23 @@
 'use client'
 
-import MainLayout from '@/components/layout/MainLayout'
 import MetamaskSteps from '@/app/register-metamask/components/MetamaskSteps'
 import { useContext } from 'react'
 import { ConfigContext } from '@/contexts/ConfigContext'
 import ImageFromJSON from '@/components/core/ImageFromJSON'
+import Header from '@/components/landingPage/Header'
+import Footer from '@/components/common/footer'
 
 export default function RegisteMetamask() {
   const { texts } = useContext(ConfigContext)
   const loginData = texts?.['register']
 
   return (
-    <MainLayout disablePadding>
-      <div className="flex flex-col min-h-screen">
-        <main className="flex flex-1">
-          {/* Lado da imagem */}
-          <div
-            className="
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        {/* Lado da imagem */}
+        <div
+          className="
               hidden md:block
               w-[300px]
               sm:w-[350px]
@@ -28,30 +29,30 @@ export default function RegisteMetamask() {
               relative
               image-large-override
             "
-          >
-            <ImageFromJSON
-              src={loginData?.image}
-              alt={loginData?.alt}
-              className="w-full h-auto object-contain"
-              width={653}
-              height={800}
-            />
-          </div>
+        >
+          <ImageFromJSON
+            src={loginData?.image}
+            alt={loginData?.alt}
+            className="w-full h-auto object-contain"
+            width={653}
+            height={800}
+          />
+        </div>
 
-          {/* Lado dos steps */}
-          <div className="flex-1 flex items-start justify-start px-4 sm:px-8 md:px-12 pt-12 pb-8">
-            <div
-              className="
+        {/* Lado dos steps */}
+        <div className="flex-1 flex items-start justify-start px-4 sm:px-8 md:px-12 pt-12 pb-8">
+          <div
+            className="
                 mt-4
                 ml-6 sm:ml-10 md:ml-14 lg:ml-20
                 max-w-md w-full
               "
-            >
-              <MetamaskSteps />
-            </div>
+          >
+            <MetamaskSteps />
           </div>
-        </main>
-      </div>
-    </MainLayout>
+        </div>
+      </main>
+      <Footer />
+    </div>
   )
 }
