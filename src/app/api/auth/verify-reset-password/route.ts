dd009d62e998312a0a2a72342d68320bcc686c7e
Samuel Santos <mucasantos@gmail.com>
2025-12-17 20:17:39 -0300

    bug password change

--- src/app/api/auth/verify-reset-password/route.ts
+++ src/app/api/auth/verify-reset-password/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: NextRequest) {
             clientId,
             email,
             otpCode,
-            password: newPassword,
+            newPassword,
         }
 
         const response = await fetch(`${apiUrl}/auth/verify-reset-password`, {
