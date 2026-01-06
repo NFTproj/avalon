51ffca6825d75c179b5a90f6994405cff733fdea
Fe_ath <felipeblocklize@gmail.com>
2026-01-06 18:34:42 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  eslint: { ignoreDuringBuilds: true }, // <-- pula erros de lint no build
   images: {
     localPatterns: [{ pathname: '/assets/**', search: '' }],
     remotePatterns: [
